fdcdcdbb643cab9e027129673ce13138056607af
Alexander Voronkov <myaddictionisguitars@gmail.com>
2018-02-13 14:46:28 +0300

    DB models

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True)
     todoist_token = db.Column(db.String(128))
+    problems = db.relationship('ProblemProbability', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Problem(db.Model):
+    num = db.Column(db.Integer)
+    body = db.Column(db.String(10000))
+    steps = db.Column(db.String(10000))
+
+
+class ProblemProbability(db.Model):
+    val = db.Column(db.Float)
+    problem_num = db.Column(db.Integer)
