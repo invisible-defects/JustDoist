03624332e65c83ceb97b48b0c8a86b51601c88ac
Alexander Voronkov <myaddictionisguitars@gmail.com>
2018-02-15 12:47:34 +0300

    Problem probabilities

--- app/models.py
+++ app/models.py
@@ -3,12 +3,18 @@ import todoist
 from flask_login import UserMixin
 
 
+def parser(problem_num, todoist_api):
+    return 0
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     todoist_token = db.Column(db.String(128))
     problems = db.relationship('ProblemProbability', backref='owner', lazy='dynamic')
     last_problem_shown = db.Column(db.DateTime())
+
+    possible_problems = [1, 2, 3, 4]
 
     def link_todoist(self, api_key):
         self.todoist_token = api_key
@@ -21,13 +27,23 @@ class User(UserMixin, db.Model):
             return False
 
     def get_problem(self):
-        problems = self.problems.query.filter_by()
+        problem = ProblemProbability.query.filter_by(
+            user_token=self.todoist_token).order_by(ProblemProbability.val.desc()).first()
+        return problem
 
     def count_probabilities(self):
-        pass
+        if not self.check_todoist():
+            return False
+        api = todoist.TodoistAPI(self.todoist_token)
+        for problem in self.possible_problems:
+            val = parser(problem, api)
+            prob = ProblemProbability(val=val, problem_num=problem, user_token=self.todoist_token, steps_completed=0)
+            db.session.add(prob)
+            db.session.commit()
+
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} {}>'.format(self.id, self.todoist_token)
 
 
 class Problem(db.Model):
