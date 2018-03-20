b2b04c201aaf4f07847b8d41b8b17533be423f34
Artem Stelzer <shtirlets@list.ru>
2018-03-20 14:28:16 +0300

    Smth from past

---
+++ index.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="shortcut icon" href="app/static/img/logo.svg">
+</head>
+
+<body>
+    
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Unica+One');
+
+    html{
+        background: #ff230c;
+    }
+    .wrapper{
+        display: block;
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform: translate(-50%,-50%);
+    }
+    .media-object{
+        display:block;
+        width:100%;
+        margin-left:auto;
+        margin-right:auto;
+        margin-bottom:30px;
+    }
+    h5{
+        padding-left:1px;
+        padding-bottom:30px
+    }
+    .cont{
+        margin-top: -10rem;
+        display: block;
+        width:auto;
+        padding:50px;
+        margin-left: auto;
+        margin-right: auto;
+        overflow-y: auto;
+        box-sizing: border-box;
+        overflow: hidden;
+        transition: .2s;
+
+    }
+    .continue{
+        color: white;
+        cursor: pointer;
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 1.42857143;
+        text-align: center;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+        touch-action: manipulation;
+    }
+    .form{
+        display: block;
+        width: 100%;
+        height: 34px;
+        padding: 6px 12px;
+        font-size: 14px;
+        line-height: 1.42857143;
+        color: #555;
+        border: 0;
+        border-bottom: 1.3px solid #ccc;
+        transition: border .2s;
+    }
+
+    .logotext{
+        user-select: none;
+        color: white;
+        font-family: 'Unica One', cursive;
+        font-weight: 400 !important;
+        text-transform: uppercase;
+        display: flex !important;
+        align-content: center;
+        align-items: center;
+        text-align: justify;
+        justify-content: center; 
+        font-size: 3rem;
+    }
+    .logotext > img{
+        user-select: none;
+        height: 3.5rem;
+        margin-top: -1.4rem;
+    }
+    button{
+        color:#e44332ff !important;
+        margin: 0 auto !important;
+        display: block !important;
+    }
+</style>
+<div class="wrapper">
+<div class="cont">
+    <div class="logotext">
+    J<img src="app/static/img/logo.svg">STDOIST
+    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/niZMzbEPIWw?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
+</div>
+</body>
+</html>
