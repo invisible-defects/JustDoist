ca74857a897241ea217e78f132beb6ee798bcea4
DeniskaMazur <denis.mazur.02@inbox.ru>
2018-05-05 22:30:20 +0300

    feat: refactor code

--- main/api_utils.py
+++ main/api_utils.py
@@ -1,47 +1,53 @@
-from todoist import TodoistAPI
+import sys; sys.path.append("../")
+from config import Config
+from main.api_wrapper import get_user_tasks
+
+from todoist.api import TodoistAPI
 
 
-def has_preferred_tasks(api: TodoistAPI) -> bool:
-    max_prior = max((item.get("priority", -1000) for item in api.items.all()), default=1)
-    return max_prior != 1
+api_token = "a7ced8b476ac01b1e96ee80fbe185f7bbea4decf"
 
 
-def does_use_regularly(api: TodoistAPI) -> float:
-    return int(
-        sum(map(lambda x: x['total_completed'],
-            api.completed.get_stats()['days_items'])) < 7
-    )
+def uses_task_grouping(api: TodoistAPI) -> bool :
+    """Detects if user uses the task grouping feature.
+    
+    Arguments:
+        api {TodoistAPI} -- todoist api
+    """
+
+    api.sync()
+
+    return len(api.state["projects"]) > 1
 
 
-def is_premium(api: TodoistAPI) -> bool:
-    return api.user.get()["is_premium"]
+def detect_exhaustion(api: TodoistAPI, threshold:int=6) -> bool:
+    """Detects if user assigns too many tasks daily
+    
+    Arguments:
+        api {TodoistAPI} -- todoist api
+        threshold {int} -- how many tasks daily are considered too much (default: {6})
+    """
+
+    tasks = get_user_tasks(api.token)
+
+    return len([task for task in tasks if task["due"]["date"] == tasks[0]["due"]["date"]]) > threshold
 
 
-def get_combined_problems(api: TodoistAPI) -> dict:
-    # is_premium(api)
-    # get_stats(api)
-    problems_dict = {}
-    problems_dict[2] = has_preferred_tasks(api)
-    problems_dict[3] = does_use_regularly(api)
-    return problems_dict
+def detect_lack_priorities(api: TodoistAPI) -> bool:
+    """Detects if user assigns priorities to their tasks
+    
+    Arguments:
+        api {TodoistAPI} -- todoist api
+    """
 
+    return len([task for task in get_user_tasks(api.token) if task["priority"] != 1]) > 0
 
-def get_stats(api: TodoistAPI) -> dict:
-    statistics = api.completed.get_stats()
-    stats_for_graph = statistics["karma_graph_data"]
+def detect_regular_use(api: TodoistAPI) -> bool:
+    """Detect if user uses Todoist regularly
+    
+    Arguments:
+        api {TodoistAPI} -- todoist api
+    """
 
-    # TODO: describe this shit
-    if (stats_for_graph[-1]["karma_avg"] < stats_for_graph[0]["karma_avg"]):
-        stats_linear_graph = -int(
-            int(stats_for_graph[-1]["karma_avg"]) /
-            int(stats_for_graph[0]["karma_avg"]) * 100
-        )
-    else:
-        stats_linear_graph = int(
-            int(stats_for_graph[0]["karma_avg"]) /
-            int(stats_for_graph[-1]["karma_avg"]) * 100
-        )
-
-    # stats_for_linear_graph_completed = sum(map(lambda x: x['total_completed'], statistics["days_items"]))
-    stats_for_graph_optimized = [s.get("kargma_avg", None) for s in stats_for_graph]
-    return {"graph": stats_for_graph_optimized, "percentage": stats_linear_graph}
+    return sum(map(lambda x: x['total_completed'],
+        api.completed.get_stats()['days_items'])) < 7
