bf2d2db38ea1cb3fd9bf362f4ca94d5719d08db6
DeniskaMazur <denis.mazur.02@inbox.ru>
2018-05-05 21:39:26 +0300

    add detecting lack of priorities

--- main/api_utils_refactor.py
+++ main/api_utils_refactor.py
@@ -5,6 +5,9 @@ from main.api_wrapper import get_user_tasks
 from todoist.api import TodoistAPI
 
 
+api_token = "a7ced8b476ac01b1e96ee80fbe185f7bbea4decf"
+
+
 def uses_task_grouping(api: TodoistAPI) -> bool :
     """Detects if user uses the task grouping feature.
     
@@ -28,3 +31,14 @@ def detect_exhaustion(api: TodoistAPI, threshold:int=6) -> bool:
     tasks = get_user_tasks(api.token)
 
     return len([task for task in tasks if task["due"]["date"] == tasks[0]["due"]["date"]]) > threshold
+
+
+def detect_lack_priorities(api: TodoistAPI) -> bool:
+    """Detects if user assigns priorities to their tasks
+    
+    Arguments:
+        api {TodoistAPI} -- todoist api
+    """
+
+    return len([task for task in get_user_tasks(api.token) if task["priority"] != 1]) > 0
+
