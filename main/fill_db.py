101f059f2e9a41fbfc8d1b45b07abb93574946c9
justpianm8 <usangeorg@ya.ru>
2018-05-06 04:21:57 +0300

    filld_db.yp

--- main/fill_db.py
+++ main/fill_db.py
@@ -1,5 +1,6 @@
 import json
 from load_django import load_django; load_django()
+
 from main.models import SuggestedProblem, ProblemStep
 
 
@@ -8,21 +9,19 @@ if __name__ == "__main__":
         problems = json.loads(f.read())
 
     for problem in problems:
-        if not SuggestedProblem.objects.filter(uid=problem['uid']):
-            continue
         suggestion = SuggestedProblem(
-            uid = problem['uid'],
-            title = problem['title'],
-            body = '\n'.join(problem['body']),
-            steps_num = problem['steps_num']
+            uid=problem['uid'],
+            title=problem['title'],
+            body='\n'.join(problem['body']),
+            steps_num=problem['steps_num']
         )
         suggestion.save()
         for step_data in problem['steps']:
             step = ProblemStep(
-                related_problem = suggestion,
-                number = step_data['number'],
-                description = step_data['description'],
-                task = step_data['task']
+                related_problem=suggestion,
+                number=step_data['number'],
+                description=step_data['description'],
+                task=step_data['task']
             )
             step.save()
     print("[INFO] SUCCESSFULLY CREATED PRESET PROBLEMS")
